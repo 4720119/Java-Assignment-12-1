3b86b29bb4e7504068a9fa8520666feac9af2708
4720119 <103705512+4720119@users.noreply.github.com>
2022-07-08 10:53:47 +0900

    Update DNA.java

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,3 +1,24 @@
 public class DNA {
+    public String str = "";
+    
+    public String getPair(){
+        String pairStr ="";
+        String pairCh ="";
+       for(int i=0;i<str.length();i++){
+           if(str.charAt(i)=='A')
+           pairCh="T";
+           
+           if(str.charAt(i)=='T')
+           pairCh="A";
+           
+           if(str.charAt(i)=='G')
+           pairCh="C";
 
+           if(str.charAt(i)=='C')
+           pairCh="G";
+           
+           pairStr +=pairCh;
+    }
+       return pairStr;
+    }
 }
